4e288895fcda980ab76388b41300a517ea9a79cc
David Wilkie <dwilkie@gmail.com>
2015-09-22 21:41:48 +0700

    Convert specs to RSpec 3.0.2 syntax with Transpec
    This conversion is done by Transpec 3.1.1 with the following command:
        transpec
    
    * 29 conversions
        from: obj.should
          to: expect(obj).to
    
    * 10 conversions
        from: =~ /pattern/
          to: match(/pattern/)
    
    * 8 conversions
        from: == expected
          to: eq(expected)
    
    * 5 conversions
        from: =~ [1, 2]
          to: match_array([1, 2])
    
    * 1 conversion
        from: it { should ... }
          to: it { is_expected.to ... }
    
    * 1 conversion
        from: it { should_not ... }
          to: it { is_expected.not_to ... }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/bongloy/spec_helpers/api_helpers_spec.rb
+++ spec/bongloy/spec_helpers/api_helpers_spec.rb
@@ -6,80 +6,80 @@ module Bongloy
     describe ApiHelpers do
       describe "#sample_customer_id(sequence = nil)" do
         it "should return a bongloy sample customer id" do
-          subject.sample_customer_id.should =~ /^cus_/
+          expect(subject.sample_customer_id).to match(/^cus_/)
         end
 
         context "passing a sequence" do
           it "should put the sequence at the end of the customer id" do
-            subject.sample_customer_id(1).should =~ /1$/
+            expect(subject.sample_customer_id(1)).to match(/1$/)
           end
         end
       end
 
       describe "#sample_token_id(sequence = nil)" do
         it "should return a bongloy sample token id" do
-          subject.sample_token_id.should =~ /^tok_/
+          expect(subject.sample_token_id).to match(/^tok_/)
         end
 
         context "passing a sequence" do
           it "should put the sequence at the end of the token id" do
-            subject.sample_token_id(1).should =~ /1$/
+            expect(subject.sample_token_id(1)).to match(/1$/)
           end
         end
       end
 
       describe "#sample_card_id(sequence = nil)" do
         it "should return a bongloy sample card id" do
-          subject.sample_card_id.should =~ /^card_/
+          expect(subject.sample_card_id).to match(/^card_/)
         end
 
         context "passing a sequence" do
           it "should put the sequence at the end of the card id" do
-            subject.sample_card_id(1).should =~ /1$/
+            expect(subject.sample_card_id(1)).to match(/1$/)
           end
         end
       end
 
       describe "#sample_charge_id(sequence = nil)" do
         it "should return a bongloy sample charge id" do
-          subject.sample_charge_id.should =~ /^ch_/
+          expect(subject.sample_charge_id).to match(/^ch_/)
         end
 
         context "passing a sequence" do
           it "should put the sequence at the end of the charge id" do
-            subject.sample_charge_id(1).should =~ /1$/
+            expect(subject.sample_charge_id(1)).to match(/1$/)
           end
         end
       end
 
       describe "#sample_balance_transaction_id(sequence = nil)" do
         it "should return a bongloy sample balance transaction id" do
-          subject.sample_balance_transaction_id.should =~ /^txn_/
+          expect(subject.sample_balance_transaction_id).to match(/^txn_/)
         end
 
         context "passing a sequence" do
           it "should put the sequence at the end of the balance transaction id" do
-            subject.sample_balance_transaction_id(1).should =~ /1$/
+            expect(subject.sample_balance_transaction_id(1)).to match(/1$/)
           end
         end
       end
 
       describe "#sample_credit_card_numbers" do
         it "should return a hash sample credit card numbers" do
-          subject.sample_credit_card_numbers.should have_key(:visa)
-          subject.sample_credit_card_numbers.should have_key(:mastercard)
+          expect(subject.sample_credit_card_numbers).to have_key(:visa)
+          expect(subject.sample_credit_card_numbers).to have_key(:mastercard)
         end
       end
 
       describe "#authentication_headers(key)" do
         it "should return HTTP headers for Bearer authentication" do
-          subject.authentication_headers("foo")["HTTP_AUTHORIZATION"].should == "Bearer foo"
+          expect(subject.authentication_headers("foo")["HTTP_AUTHORIZATION"]).to eq("Bearer foo")
         end
       end
 
       describe "#asserted_authentication_headers(key)" do
         it "should return the asserted HTTP headers for Bearer authentication" do
-          subject.asserted_authentication_headers("foo")["Authorization"].should == "Bearer foo"
+          expect(subject.asserted_authentication_headers("foo")["Authorization"]).to eq("Bearer foo")
         end
       end
 
@@ -92,60 +92,60 @@ module Bongloy
 
       describe "#credit_card_token_params(options = {})" do
         it "should return a set of token params suitable for creating credit card tokens" do
-          subject.credit_card_token_params.should have_key(:card)
+          expect(subject.credit_card_token_params).to have_key(:card)
         end
       end
 
       describe "#wing_card_token_params(options = {})" do
         it "should return a set of token params suitable for creating wing card tokens" do
-          subject.wing_card_token_params.should have_key(:card)
+          expect(subject.wing_card_token_params).to have_key(:card)
         end
       end
 
       describe "#sample_credit_card(options = {})" do
         it "should return a sample response for a credit card" do
-          subject.sample_credit_card.keys.should =~ [
+          expect(subject.sample_credit_card.keys).to match_array([
             "id", "object", "last4", "brand", "fingerprint", "customer", "country",
             "created", "address_line1_check", "address_zip_check", "cvc_check",
             "exp_month", "exp_year", "name", "address_line1", "address_line2", "address_city",
             "address_state", "address_zip", "address_country"
-          ]
+          ])
         end
       end
 
       describe "#sample_wing_card(options = {})" do
         it "should return a sample response for a credit card" do
-          subject.sample_wing_card.keys.should =~ ["address_city", "address_country",
+          expect(subject.sample_wing_card.keys).to match_array(["address_city", "address_country",
             "address_line1", "address_line2", "address_state", "address_zip",
             "country", "created", "customer", "exp_month", "exp_year",
             "fingerprint", "id", "name", "object", "last4", "brand"
-          ]
+          ])
         end
       end
 
       describe "#sample_customer(options = {})" do
         it "should return a sample response for a customer" do
-          subject.sample_customer.keys.should =~ [
+          expect(subject.sample_customer.keys).to match_array([
             "id", "object", "created", "livemode", "description", "email", "default_source"
-          ]
+          ])
         end
       end
 
       describe "#sample_charge(options = {})" do
         it "should return a sample response for a charge" do
-          subject.sample_charge.keys.should =~ [
+          expect(subject.sample_charge.keys).to match_array([
             "id", "object", "created", "livemode", "description",
             "amount", "currency", "source", "captured", "balance_transaction", "customer"
-          ]
+          ])
         end
       end
 
       describe "#sample_balance_transaction(options = {})" do
         it "should return a sample response for a balance transaction" do
-          subject.sample_balance_transaction.keys.should =~ [
+          expect(subject.sample_balance_transaction.keys).to match_array([
             "id", "object", "created", "amount", "currency",
             "type", "source", "available_on", "status"
-          ]
+          ])
         end
       end
 
@@ -154,13 +154,13 @@ module Bongloy
           subject { described_class.new(:api_endpoint => ENV["STRIPE_API_ENDPOINT"]) }
 
           it "should return :post" do
-            subject.update_customer_http_method.should == :post
+            expect(subject.update_customer_http_method).to eq(:post)
           end
         end
 
         context "bongloy is the endpoint" do
           it "should return :put" do
-            subject.update_customer_http_method.should == :put
+            expect(subject.update_customer_http_method).to eq(:put)
           end
         end
       end
@@ -168,13 +168,13 @@ module Bongloy
       describe "#create_token_headers" do
         context "passing {'X-CUSTOM-HEADER' => 'foo'}" do
           it "should return the custom headers" do
-            subject.create_token_headers({'X-CUSTOM-HEADER' => 'foo'}).should == {'X-CUSTOM-HEADER' => 'foo'}
+            expect(subject.create_token_headers({'X-CUSTOM-HEADER' => 'foo'})).to eq({'X-CUSTOM-HEADER' => 'foo'})
           end
         end
 
         context "passing no args" do
           it "should return authorization headers" do
-            subject.create_token_headers.should == {"Authorization" => "Bearer "}
+            expect(subject.create_token_headers).to eq({"Authorization" => "Bearer "})
           end
         end
       end
@@ -182,11 +182,11 @@ module Bongloy
       describe "#stripe_mode?" do
         context "the endpoint is stripe" do
           subject { described_class.new(:api_endpoint => ENV["STRIPE_API_ENDPOINT"]) }
-          it { should be_stripe_mode }
+          it { is_expected.to be_stripe_mode }
         end
 
         context "the endpoint is not stripe" do
-          it { should_not be_stripe_mode }
+          it { is_expected.not_to be_stripe_mode }
         end
       end
 
@@ -197,16 +197,16 @@ module Bongloy
           let(:charge_params_options) { { } }
 
           it "should return no customer or card params" do
-            result.should have_key("amount")
-            result.should have_key("currency")
+            expect(result).to have_key("amount")
+            expect(result).to have_key("currency")
           end
 
           context "passing other options" do
             let(:charge_params_options) { { "amount" => "4000", "currency" => "khr" } }
 
             it "should override the default params" do
-              result["amount"].should == "4000"
-              result["currency"].should == "khr"
+              expect(result["amount"]).to eq("4000")
+              expect(result["currency"]).to eq("khr")
             end
           end
         end
